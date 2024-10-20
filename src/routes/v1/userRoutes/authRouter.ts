d83069a6a0f727ded0f61b456a7b214f6cceee77
Ajay <bointldev@gmail.com>
2024-10-20 10:55:24 +0530

    user email verification

--- src/routes/v1/userRoutes/authRouter.ts
+++ src/routes/v1/userRoutes/authRouter.ts
@@ -1,5 +1,13 @@
 import authController from '../../../controllers/users/authController';
+import auth from '../../../middleware/auth';
 
 const router = require('express').Router();
 router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.post('/refresh-tokens', authController.refreshTokens);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+router.post('/send-verification-email', auth(), authController.sendVerificationEmail);
+
 export default router;
